96a63734a3e4af569fb83c63ebdab47df67a08a4
David <dapalma94@gmail.com>
2026-08-13 17:10:35 -0500

    feat: register repositories with dependency injection

--- TaskManager.Api/Program.cs
+++ TaskManager.Api/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
+using TaskManager.Application.Interfaces;
 using TaskManager.Infrastructure.Data;
+using TaskManager.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +11,10 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<TaskManagerDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<ITaskRepository, TaskRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<ITaskStatusHistoryRepository, TaskStatusHistoryRepository>();
 
 var app = builder.Build();
 
